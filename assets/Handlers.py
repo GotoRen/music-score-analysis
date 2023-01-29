1736fb598b2e4712dac21a13eab56458da322039
kenshin1025 <sukoyakamind1025@gmail.com>
2023-01-29 21:14:24 +0900

    各音符の音階を取得してコンソールに表示できるようになった

--- assets/Handlers.py
+++ assets/Handlers.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 WINDOW_NAME_INPUT = "input"
 WINDOW_NAME_OUTPUT = "output"
@@ -32,27 +33,55 @@ class ManipulateImage:
         print(img_size)
 
         # grayscale conversion
+        # 白黒画像に変換
         gray_img = cv2.cvtColor(src_img, cv2.COLOR_RGB2GRAY)
         # FileHandler.output_image(WINDOW_NAME_DEBUG, gray_img)
 
+        # 画像処理では白い部分に物がある扱いになるので，音符や五線譜が白くなるように白黒反転
         bitwise_gray_img = cv2.bitwise_not(gray_img)
         FileHandler.output_image("bitwise_gray_img", bitwise_gray_img)
 
+        # 大津の２値化で0か255に変換
         retval, threshold_img = cv2.threshold(bitwise_gray_img, 240, 255, cv2.THRESH_OTSU)
         FileHandler.output_image("threshold_img", threshold_img)
 
+        # 各音符のエッジを検出
         contours, hierarchy = ManipulateImage.find_notes(src_img, threshold_img)
+        # エッジから各音符の重心座標を取得
         notes_xy_arr = ManipulateImage.get_scores_xy_array(contours)
 
+        # 五線譜の線の高さの配列を取得
         score_lines_rho_arr = ManipulateImage.get_score_lines_rho_array(src_img, threshold_img)
-
 
         score_list= ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"]
         # 線の外のドや線と線の間の音符を検出するためにちょっと加工する
         # 五線譜を下から見るために逆順にする
         score_lines_rho_arr.reverse()
         between_lines_length = abs(score_lines_rho_arr[0]-score_lines_rho_arr[1])
+        between_lines_half_length = math.floor(between_lines_length/2)
         score_lines_rho_arr.insert(0, score_lines_rho_arr[0]+between_lines_length)
+        # 上のコードの時点で配列に「ドミソシレファ」に当たる高さが入っている．間が足りないので埋める．
+        # 方法は線と線の間の長さの半分を各要素に足した値を間に入れる(下から見ていってるからほんとは引く必要があった．)
+        score_height_base = []
+        for item in score_lines_rho_arr:
+            score_height_base.append(item)
+            score_height_base.append(item-between_lines_half_length)
+        # 最後に一番下のドと同じ立ち位置の一番上のラを追加する
+        score_height_base.append(score_height_base[-1]-between_lines_half_length)
+        print(score_height_base)
+
+        for note_index, note in enumerate(notes_xy_arr):
+            min = 100
+            min_index = 0
+            for index, height in enumerate(score_height_base):
+                diff = abs(height - note[1])
+                if diff < min:
+                    min = diff
+                    min_index = index
+            print(f'{note_index}番目の音符は, {score_list[min_index%7]}')
+
+
+
 
         # # retval, dst = cv2.threshold(gray_img, 190, 255, cv2.THRESH_TOZERO_INV)
         # dst = cv2.bitwise_not(dst)
